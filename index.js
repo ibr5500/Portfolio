bc43bd224f7237368d9759147a41cff90289de7f
sarazaToussaint <tmsaraza@gmail.com>
2022-04-04 15:25:49 +0200

    Add Eslint and index.js file

---
+++ index.js
@@ -0,0 +1,10 @@
+function openMenu() {
+  const x = document.getElementById('mob-menu');
+  if (x.style.display === 'block') {
+    x.style.display = 'none';
+  } else {
+    x.style.display = 'block';
+  }
+}
+
+openMenu();
