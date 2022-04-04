b755db3af2480aa5556179b8a3184ec8bdce3a0b
ibr5500 <ibr.5500@gmail.com>
2022-04-04 20:53:10 +0300

    Fixed Webhint & Eslint errors

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 document.getElementById('menu-button').onclick = function openMenu() {
-    if (document.getElementById('mobile-menu').style != 'display: block') {
-        document.getElementById('mobile-menu').style = 'display: block';
-        document.getElementById('menu-button').style = 'display: none';
-    }
-
+  if (document.getElementById('mobile-menu').style !== 'display: block') {
+    document.getElementById('mobile-menu').style = 'display: block';
+    document.getElementById('menu-button').style = 'display: none';
+  }
 };
 
 document.getElementById('close').onclick = function openMenu() {
-    if (document.getElementById('mobile-menu').style != 'display: none') {
-        document.getElementById('mobile-menu').style = 'display: none';
-        document.getElementById('menu-button').style = 'display: block';
-    }
+  if (document.getElementById('mobile-menu').style !== 'display: none') {
+    document.getElementById('mobile-menu').style = 'display: none';
+    document.getElementById('menu-button').style = 'display: block';
+  }
 };
