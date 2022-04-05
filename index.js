8f27659a0ff2dd54ef94d1b2476a8fe91483d049
ibr5500 <ibr.5500@gmail.com>
2022-04-06 00:22:27 +0300

    Added the backdrop filter for the mobile menu

--- index.js
+++ index.js
@@ -3,21 +3,22 @@ const closeButton = document.querySelector('.close');
 const closeItems = document.querySelector('.closeItem');
 
 openButton.onclick = function openMenu() {
-  document.querySelector('#mobile-menu').style = 'display: block';
-  document.querySelector('#menu-button').style = 'display: none';
-  document.querySelector('body').style = 'overflow: hidden';
-  document.querySelector('#home').style = 'backdrop-filter: blur(35px)';
-  document.querySelector('#home').style = 'z-index: -1;';
+    document.querySelector('#mobile-menu').style = 'display: block';
+    document.querySelector('#menu-button').style = 'display: none';
+    document.querySelector('body').style = 'overflow: hidden';
+    document.querySelector('main').style = 'filter: blur(6px); z-index: -1';
 };
 
 closeButton.onclick = function closeMenu() {
-  document.querySelector('#mobile-menu').style = 'display: none';
-  document.querySelector('#menu-button').style = 'display: block';
-  document.querySelector('body').style = 'overflow: scroll';
+    document.querySelector('#mobile-menu').style = 'display: none';
+    document.querySelector('#menu-button').style = 'display: block';
+    document.querySelector('body').style = 'overflow: scroll';
+    document.querySelector('main').style = 'filter: none; z-index: 0';
 };
 
 closeItems.onclick = function closeItem() {
-  document.querySelector('#mobile-menu').style = 'display: none';
-  document.querySelector('#menu-button').style = 'display: block';
-  document.querySelector('body').style = 'overflow: scroll';
+    document.querySelector('#mobile-menu').style = 'display: none';
+    document.querySelector('#menu-button').style = 'display: block';
+    document.querySelector('body').style = 'overflow: scroll';
+    document.querySelector('main').style = 'filter: none; z-index: 0';
 };
